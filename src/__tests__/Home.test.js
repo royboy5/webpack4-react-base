e99602838c1a6f4846773e0519e71ab5eb65c2af
Roy <roye19@gmail.com>
2018-08-21 02:14:13 -0400

    refactored tests

--- src/__tests__/Home.test.js
+++ src/__tests__/Home.test.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 
-import Home from '../pages/Home'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
+import Home from '@pages/Home'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+
+let wrapped
+
+beforeEach(() => {
+  wrapped = shallow(<Home />)
+})
 
 it('Shows a Header', () => {
-  const wrapped = shallow(<Home />)
-
   expect(wrapped.find(Header).length).toEqual(1)
 })
 
 it('Shows a Footer', () => {
-  const wrapped = shallow(<Home />)
-
   expect(wrapped.find(Footer).length).toEqual(1)
 })
